bf7960965af3467cd13502006d02d215cb7bf4eb
Mitchell Couturier <mitchellcouturier@MacBook-Pro.local>
2017-09-19 19:08:19 -0400

    hw 3 init

---
+++ HW3/HW3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW3
+//
+//  Created by Mitchell Couturier on 9/19/17.
+//  Copyright © 2017 CouturierCrowe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
