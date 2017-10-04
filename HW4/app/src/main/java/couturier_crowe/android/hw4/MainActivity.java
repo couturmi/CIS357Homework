502af96db50e7913cbc5104ddf059bd73fdbd622
caitlincrowe3 <caitlincrowe3@yahoo.com>
2017-10-04 01:55:14 -0400

    XML design and Lambda skeletons for part 1 are done

--- HW4/app/src/main/java/couturier_crowe/android/hw4/MainActivity.java
+++ HW4/app/src/main/java/couturier_crowe/android/hw4/MainActivity.java
@@ -2,6 +2,8 @@ package couturier_crowe.android.hw4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText latitudep1 = (EditText) findViewById(R.id.latitudep1);
+        EditText latitudep2 = (EditText) findViewById(R.id.latitudep2);
+        EditText longitudep1 = (EditText) findViewById(R.id.longitudep1);
+        EditText longitudep2 = (EditText) findViewById(R.id.longitudep2);
+
+        Button calculate = (Button) findViewById(R.id.calculateButton);
+        Button clear = (Button) findViewById(R.id.clearButton);
+
+        calculate.setOnClickListener(v -> {
+            //calculate and display the distance and bearings
+        });
+
+        clear.setOnClickListener(v -> {
+            //clear the EditTexts
+        });
+
     }
 }
