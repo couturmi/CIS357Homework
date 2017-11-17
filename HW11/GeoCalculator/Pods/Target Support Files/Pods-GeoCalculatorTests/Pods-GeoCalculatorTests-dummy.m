aa935b5826056c1fbe4535c75281c978ed6eced3
Mitchell John Couturier <couturmi@mail.gvsu.edu>
2017-11-17 00:25:08 -0500

    remove iOS / add Android / finish HW11

--- HW11/GeoCalculator/Pods/Target Support Files/Pods-GeoCalculatorTests/Pods-GeoCalculatorTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GeoCalculatorTests : NSObject
-@end
-@implementation PodsDummy_Pods_GeoCalculatorTests
-@end
---
+++ HW11/ic-hw10/app/src/main/java/cis/gvsu/edu/geocalculator/HistoryActivity.java
@@ -0,0 +1,41 @@
+package cis.gvsu.edu.geocalculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import org.parceler.Parcels;
+
+public class HistoryActivity extends AppCompatActivity  implements HistoryFragment.OnListFragmentInteractionListener  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    @Override
+    public void onListFragmentInteraction(LocationLookup item) {
+        System.out.println("Interact!");
+        Intent intent = new Intent();
+        Parcelable wrapped = Parcels.wrap(item);
+        intent.putExtra("item", wrapped);
+        setResult(MainActivity.HISTORY_RESULT,intent);
+        finish();
+    }
+}
